181151db263ebdf59cd2349dc40191a1bcde03fc
erasebegin <chris.james.haupt@gmail.com>
2026-04-07 11:00:21 +0200

    SEO optimisation, styling improvements, location page filter improvements

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Cafe Review';
+export const SITE_DESCRIPTION = 'Honest cafe reviews across Europe — discover the best coffee, vegan options, workability, and more.';
