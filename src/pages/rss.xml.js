9a242c80891e06ab8fc2046ee7f1eea1d24ff45a
erasebegin <chris.james.haupt@gmail.com>
2026-05-08 21:04:41 +0200

    fix RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,19 @@
-import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { getAllBlogPosts } from '../lib/sanity-utils';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = await getAllBlogPosts();
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
+		trailingSlash: true,
 		items: posts.map((post) => ({
-			...post.data,
-			link: `/cafe/${post.id}/`,
+			title: post.title,
+			description: post.seoDescription || post.description || '',
+			link: `/cafe/${post.slug}/`,
+			pubDate: post.pubDate,
 		})),
 	});
 }
